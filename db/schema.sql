6253ae01392c00cb598d0c8d18d0b920d2de03d1
ElmasSayed <elmassayed@gmail.com>
2017-05-27 18:23:14 -0700

    Jaws DB resolved

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,22 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
 
+--LOCAL
+CREATE DATABASE burgers_db; --local
+USE burgers_db; --local db
+
+--HEROKU/JAWS
+--DB is automatically created by Heroku Jaws with names like "sjrp8qw9yxmskqp8" 
+--use sjrp8qw9yxmskqp8 --Jaws db
+
+
+--COMMON for local and Heroku
 CREATE TABLE burgers
 (
 	id int AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
-    devoured boolean,
+    devoured boolean not null default 0,
+    createdAt timestamp not null default current_timestamp,
+    updatedAt timestamp not null default current_timestamp,
 	PRIMARY KEY (id)
 );
 
+select * from burgers
